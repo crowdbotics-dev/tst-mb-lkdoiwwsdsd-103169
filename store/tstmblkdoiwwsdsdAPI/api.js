8f1faaecb4a72453a875f07089d145d2967a481e
Crowdbotics <engineering@crowdbotics.com>
2023-09-15 12:43:59 +0000

    Update store.

--- store/tstmblkdoiwwsdsdAPI/api.js
+++ store/tstmblkdoiwwsdsdAPI/api.js
@@ -1,144 +1,109 @@
-import axios from "axios";
+import axios from "axios"
 const tstmblkdoiwwsdsdAPI = axios.create({
   baseURL: "https://tst-mb-lkdoiwwsdsd-103169.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return tstmblkdoiwwsdsdAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_hello_list(payload) {
-  return tstmblkdoiwwsdsdAPI.get(`/api/v1/hello/`);
+  return tstmblkdoiwwsdsdAPI.get(`/api/v1/hello/`)
 }
-
 function api_v1_hello_create(payload) {
-  return tstmblkdoiwwsdsdAPI.post(`/api/v1/hello/`, payload);
+  return tstmblkdoiwwsdsdAPI.post(`/api/v1/hello/`, payload)
 }
-
 function api_v1_hello_retrieve(payload) {
-  return tstmblkdoiwwsdsdAPI.get(`/api/v1/hello/${payload.id}/`);
+  return tstmblkdoiwwsdsdAPI.get(`/api/v1/hello/${payload.id}/`)
 }
-
 function api_v1_hello_update(payload) {
-  return tstmblkdoiwwsdsdAPI.put(`/api/v1/hello/${payload.id}/`, payload);
+  return tstmblkdoiwwsdsdAPI.put(`/api/v1/hello/${payload.id}/`, payload)
 }
-
 function api_v1_hello_partial_update(payload) {
-  return tstmblkdoiwwsdsdAPI.patch(`/api/v1/hello/${payload.id}/`, payload);
+  return tstmblkdoiwwsdsdAPI.patch(`/api/v1/hello/${payload.id}/`, payload)
 }
-
 function api_v1_hello_destroy(payload) {
-  return tstmblkdoiwwsdsdAPI.delete(`/api/v1/hello/${payload.id}/`);
+  return tstmblkdoiwwsdsdAPI.delete(`/api/v1/hello/${payload.id}/`)
 }
-
 function api_v1_helloo_list(payload) {
-  return tstmblkdoiwwsdsdAPI.get(`/api/v1/helloo/`);
+  return tstmblkdoiwwsdsdAPI.get(`/api/v1/helloo/`)
 }
-
 function api_v1_helloo_create(payload) {
-  return tstmblkdoiwwsdsdAPI.post(`/api/v1/helloo/`, payload);
+  return tstmblkdoiwwsdsdAPI.post(`/api/v1/helloo/`, payload)
 }
-
 function api_v1_helloo_retrieve(payload) {
-  return tstmblkdoiwwsdsdAPI.get(`/api/v1/helloo/${payload.id}/`);
+  return tstmblkdoiwwsdsdAPI.get(`/api/v1/helloo/${payload.id}/`)
 }
-
 function api_v1_helloo_update(payload) {
-  return tstmblkdoiwwsdsdAPI.put(`/api/v1/helloo/${payload.id}/`, payload);
+  return tstmblkdoiwwsdsdAPI.put(`/api/v1/helloo/${payload.id}/`, payload)
 }
-
 function api_v1_helloo_partial_update(payload) {
-  return tstmblkdoiwwsdsdAPI.patch(`/api/v1/helloo/${payload.id}/`, payload);
+  return tstmblkdoiwwsdsdAPI.patch(`/api/v1/helloo/${payload.id}/`, payload)
 }
-
 function api_v1_helloo_destroy(payload) {
-  return tstmblkdoiwwsdsdAPI.delete(`/api/v1/helloo/${payload.id}/`);
+  return tstmblkdoiwwsdsdAPI.delete(`/api/v1/helloo/${payload.id}/`)
 }
-
 function api_v1_hi_list(payload) {
-  return tstmblkdoiwwsdsdAPI.get(`/api/v1/hi/`);
+  return tstmblkdoiwwsdsdAPI.get(`/api/v1/hi/`)
 }
-
 function api_v1_hi_create(payload) {
-  return tstmblkdoiwwsdsdAPI.post(`/api/v1/hi/`, payload);
+  return tstmblkdoiwwsdsdAPI.post(`/api/v1/hi/`, payload)
 }
-
 function api_v1_hi_retrieve(payload) {
-  return tstmblkdoiwwsdsdAPI.get(`/api/v1/hi/${payload.id}/`);
+  return tstmblkdoiwwsdsdAPI.get(`/api/v1/hi/${payload.id}/`)
 }
-
 function api_v1_hi_update(payload) {
-  return tstmblkdoiwwsdsdAPI.put(`/api/v1/hi/${payload.id}/`, payload);
+  return tstmblkdoiwwsdsdAPI.put(`/api/v1/hi/${payload.id}/`, payload)
 }
-
 function api_v1_hi_partial_update(payload) {
-  return tstmblkdoiwwsdsdAPI.patch(`/api/v1/hi/${payload.id}/`, payload);
+  return tstmblkdoiwwsdsdAPI.patch(`/api/v1/hi/${payload.id}/`, payload)
 }
-
 function api_v1_hi_destroy(payload) {
-  return tstmblkdoiwwsdsdAPI.delete(`/api/v1/hi/${payload.id}/`);
+  return tstmblkdoiwwsdsdAPI.delete(`/api/v1/hi/${payload.id}/`)
 }
-
 function api_v1_login_create(payload) {
-  return tstmblkdoiwwsdsdAPI.post(`/api/v1/login/`, payload);
+  return tstmblkdoiwwsdsdAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return tstmblkdoiwwsdsdAPI.post(`/api/v1/signup/`, payload);
+  return tstmblkdoiwwsdsdAPI.post(`/api/v1/signup/`, payload)
 }
-
 function rest_auth_login_create(payload) {
-  return tstmblkdoiwwsdsdAPI.post(`/rest-auth/login/`, payload);
+  return tstmblkdoiwwsdsdAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return tstmblkdoiwwsdsdAPI.get(`/rest-auth/logout/`);
+  return tstmblkdoiwwsdsdAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return tstmblkdoiwwsdsdAPI.post(`/rest-auth/logout/`);
+  return tstmblkdoiwwsdsdAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return tstmblkdoiwwsdsdAPI.post(`/rest-auth/password/change/`, payload);
+  return tstmblkdoiwwsdsdAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return tstmblkdoiwwsdsdAPI.post(`/rest-auth/password/reset/`, payload);
+  return tstmblkdoiwwsdsdAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return tstmblkdoiwwsdsdAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return tstmblkdoiwwsdsdAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return tstmblkdoiwwsdsdAPI.post(`/rest-auth/registration/`, payload);
+  return tstmblkdoiwwsdsdAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return tstmblkdoiwwsdsdAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return tstmblkdoiwwsdsdAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    payload
+  )
 }
-
 function rest_auth_user_retrieve(payload) {
-  return tstmblkdoiwwsdsdAPI.get(`/rest-auth/user/`);
+  return tstmblkdoiwwsdsdAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return tstmblkdoiwwsdsdAPI.put(`/rest-auth/user/`, payload);
+  return tstmblkdoiwwsdsdAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return tstmblkdoiwwsdsdAPI.patch(`/rest-auth/user/`, payload);
+  return tstmblkdoiwwsdsdAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_hello_list,
@@ -172,4 +137,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
